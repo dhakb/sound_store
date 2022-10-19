74fcaac13515e49547be964abe1cd7f782dbacf5
giguchi <hil.giga@gmail.com>
2022-10-19 20:41:59 +0100

    add CategoriesPreview route

---
+++ src/routes/categoriesPreview/CategoriesPreview.component.jsx
@@ -0,0 +1,21 @@
+import {useContext} from "react";
+
+import {CategoriesContext} from "../../context/Categories.context";
+
+import CategoryPreview from "../../components/categoryPreview/CategoryPreview.component";
+
+const CategoriesPreview = () => {
+    const categoriesMap = useContext(CategoriesContext)
+    console.log(categoriesMap)
+
+    return (<div>
+        {
+            Object.keys(categoriesMap).map(title => {
+                const products = categoriesMap[title]
+                return <CategoryPreview products={products} key={title} title={title}/>
+            })
+        }
+    </div>)
+}
+
+export default CategoriesPreview
