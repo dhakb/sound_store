83c465de7858746c0016924ca2c994a23dc84659
giguchi <hil.giga@gmail.com>
2022-10-30 23:46:07 +0000

    create animations directory and add SoundWave component

---
+++ src/components/animations/SoundWave.component.jsx
@@ -0,0 +1,30 @@
+import "./SoundWave.styles.scss"
+
+
+const SoundWave = () => {
+    return (
+        <div className="waveAnimation">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
+
+
+export default SoundWave
